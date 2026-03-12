ce0590d5cec384bc86c4a8e39c5807aee550a9b4
HectorGuerero <hectorc0822@gmail.com>
2026-03-12 15:35:05 -0500

    feat: eliminar archivos de prueba y corregir la generación de nombres de usuario únicos en el servicio de usuarios

--- test_roles.php
+++
@@ -1,4 +0,0 @@
-<?php
-$userRoleArr = [['id' => 1, 'name' => 'admin'], ['id' => 2, 'name' => 'admin']];
-$uniqueRoles = is_array($userRoleArr) ? array_unique(array_column($userRoleArr, "name")) : [];
-print_r($uniqueRoles);
--- test_roles2.php
+++
@@ -1,4 +0,0 @@
-<?php
-$userRoleArr = ['admin', 'admin'];
-$uniqueRoles = array_unique(is_array($userRoleArr) && isset($userRoleArr[0]) && is_array($userRoleArr[0]) ? array_column($userRoleArr, "name") : $userRoleArr);
-print_r($uniqueRoles);
